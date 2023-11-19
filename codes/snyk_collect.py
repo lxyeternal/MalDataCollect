bc4e199fcac2b080a3ad8c84fb2a4e58702784d2
Blue <1796369212@qq.com>
2023-11-19 17:39:12 +0800

    framework

---
+++ codes/snyk_collect.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# @File     : snyk_collect.py
+# @Project  : MalDataCollect
+# Time      : 2023/11/19 17:38
+# Author    : honywen
+# version   : python 3.8
+# Description：
+"""
