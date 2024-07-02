98c270139f30f472aef3856ace761495e8c08f7e
Blue <1796369212@qq.com>
2024-07-02 17:10:07 +0800

    Update count_files.py

--- codes/count_files.py
+++ codes/count_files.py
@@ -12,16 +12,21 @@
 
 import os
 
-def count_files():
-    count = 0
+def count_packages_and_versions(directory):
+    package_count = 0
+    version_count = 0
+
+    for package_name in os.listdir(directory):
+        package_path = os.path.join(directory, package_name)
+        if os.path.isdir(package_path):
+            package_count += 1
+            version_count += len(os.listdir(package_path))
+
+    return package_count, version_count
+
+if __name__ == "__main__":
     directory = "/Users/blue/Documents/GitHub/pypi_malregistry"
-    packages = os.listdir(directory)
-    for package in packages:
-        package_dir = os.path.join(directory, package)
-        if os.path.isdir(package_dir):
-            versions = os.listdir(package_dir)
-            count += len(versions)
-    print(count)
+    package_count, version_count = count_packages_and_versions(directory)
+    print(f"Total number of package files: {package_count}")
+    print(f"Total number of version files: {version_count}")
 
-
-count_files()
