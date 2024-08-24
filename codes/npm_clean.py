4f6d13d7bb3f99c1d2a272e4a112167daf38b9c0
Blue <1796369212@qq.com>
2024-08-24 12:14:55 +0800

    Update npm_clean.py

--- codes/npm_clean.py
+++ codes/npm_clean.py
@@ -15,28 +15,31 @@ import shutil
 
 
 def clean_package_folders(base_folder):
-    # 遍历包名文件夹
+    # Traverse package directories
     for package_name in os.listdir(base_folder):
         package_path = os.path.join(base_folder, package_name)
 
-        # 确保当前路径是一个文件夹
+        # Ensure the current path is a directory
         if os.path.isdir(package_path):
-            # 获取版本号文件夹列表
+            # Get a list of version folders
             versions = [v for v in os.listdir(package_path) if os.path.isdir(os.path.join(package_path, v))]
+            # Filter versions that start with '0.0.1-security'
+            security_versions = [v for v in versions if v.startswith("0.0.1-security")]
 
-            # 如果只有一个版本且是0.0.1-security，删除整个包名文件夹
-            if len(versions) == 1 and versions[0] == "0.0.1-security.4":
+            # If there's only one version and it starts with '0.0.1-security', delete the entire package directory
+            if len(versions) == 1 and security_versions:
                 print(f"Deleting package folder: {package_path}")
                 shutil.rmtree(package_path)
-            # 如果有多个版本且包含0.0.1-security，删除0.0.1-security版本文件夹
-            elif "0.0.1-security" in versions:
-                version_path = os.path.join(package_path, "0.0.1-security.4")
-                print(f"Deleting version folder: {version_path}")
-                shutil.rmtree(version_path)
+            # If there are multiple versions and any version starts with '0.0.1-security', delete those version folders
+            elif security_versions:
+                for version in security_versions:
+                    version_path = os.path.join(package_path, version)
+                    print(f"Deleting version folder: {version_path}")
+                    shutil.rmtree(version_path)
             else:
                 print(f"Skipping package: {package_name}, versions: {versions}")
 
 
-# 使用函数
+# Usage of the function
 base_folder = "/Users/blue/Documents/MalDataset/npm"
 clean_package_folders(base_folder)
